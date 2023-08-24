8d5f6d630e6bead5c778703962380249878eb24b
lyma12 <79680072+lyma12@users.noreply.github.com>
2023-08-24 15:21:30 +0700

    Add files via upload

---
+++ TilemapVisualizer.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapVisualizer : MonoBehaviour
+{
+    
+    [SerializeField]
+    private Tilemap floorTilemap, wallTilemap, iconTilemap;
+    [SerializeField]
+    private TileBase floorTile, wallTop, wallSideRight, wallSiderLeft, wallBottom, wallFull, 
+        wallInnerCornerDownLeft, wallInnerCornerDownRight, 
+        wallDiagonalCornerDownRight, wallDiagonalCornerDownLeft, wallDiagonalCornerUpRight, wallDiagonalCornerUpLeft;
+    [SerializeField]
+    private List<TileBase> iconCornerSingle = new List<TileBase>(), iconSingle = new List<TileBase>(), iconSingleOneLose = new List<TileBase>();
+
+
+    public void PaintFloorTiles(IEnumerable<Vector2Int> floorPositions)
+    {
+        PaintTiles(floorPositions, floorTilemap, floorTile);
+    }
+
+
+    private void PaintTiles(IEnumerable<Vector2Int> positions, Tilemap tilemap, TileBase tile)
+    {
+        foreach (var position in positions)
+        {
+            PaintSingleTile(tilemap, tile, position);
+        }
+    }
+
+    internal void PaintSingleBasicWall(Vector2Int position, string binaryType)
+    {
+        int typeAsInt = Convert.ToInt32(binaryType, 2);
+        TileBase tile = null;
+        if (WallTypesHelper.wallTop.Contains(typeAsInt))
+        {
+           
+            tile = wallTop;
+        }else if (WallTypesHelper.wallSideRight.Contains(typeAsInt))
+        {
+            
+            tile = wallSideRight;
+        }
+        else if (WallTypesHelper.wallSideLeft.Contains(typeAsInt))
+        {
+            tile = wallSiderLeft;
+        }
+        else if (WallTypesHelper.wallBottm.Contains(typeAsInt))
+        {
+            tile = wallBottom;
+        }
+        else if (WallTypesHelper.wallFull.Contains(typeAsInt))
+        {
+            tile = wallFull;
+        }
+
+        if (tile!=null)
+            PaintSingleTile(wallTilemap, tile, position);
+    }
+
+    private void PaintSingleTile(Tilemap tilemap, TileBase tile, Vector2Int position)
+    {
+        var tilePosition = tilemap.WorldToCell((Vector3Int)position);
+        tilemap.SetTile(tilePosition, tile);
+    }
+
+    public void Clear()
+    {
+        iconTilemap.ClearAllTiles();
+        floorTilemap.ClearAllTiles();
+        wallTilemap.ClearAllTiles();
+    }
+
+    internal void PaintSingleCornerWall(Vector2Int position, string binaryType)
+    {
+        int typeASInt = Convert.ToInt32(binaryType, 2);
+        TileBase tile = null;
+
+        if (WallTypesHelper.wallInnerCornerDownLeft.Contains(typeASInt))
+        {
+            tile = wallInnerCornerDownLeft;
+        }
+        else if (WallTypesHelper.wallInnerCornerDownRight.Contains(typeASInt))
+        {
+            tile = wallInnerCornerDownRight;
+        }
+        else if (WallTypesHelper.wallDiagonalCornerDownLeft.Contains(typeASInt))
+        {
+            tile = wallDiagonalCornerDownLeft;
+        }
+        else if (WallTypesHelper.wallDiagonalCornerDownRight.Contains(typeASInt))
+        {
+            tile = wallDiagonalCornerDownRight;
+        }
+        else if (WallTypesHelper.wallDiagonalCornerUpRight.Contains(typeASInt))
+        {
+            tile = wallDiagonalCornerUpRight;
+        }
+        else if (WallTypesHelper.wallDiagonalCornerUpLeft.Contains(typeASInt))
+        {
+            tile = wallDiagonalCornerUpLeft;
+        }
+        else if (WallTypesHelper.wallFullEightDirections.Contains(typeASInt))
+        {
+            tile = wallFull;
+        }
+        else if (WallTypesHelper.wallBottmEightDirections.Contains(typeASInt))
+        {
+            tile = wallBottom;
+        }
+
+        if (tile != null)
+            PaintSingleTile(wallTilemap, tile, position);
+    }
+    internal void PaintSingleIcon(Vector2Int position, string binaryType)
+    {
+        int typeAsInt = Convert.ToInt32(binaryType, 2);
+        TileBase tile = null;
+        int choose = UnityEngine.Random.Range(0, 20);
+        int number;
+        if (IconTypesHelper.singleCorner.Contains(typeAsInt))
+        {
+            number = UnityEngine.Random.Range(0, iconCornerSingle.Count);
+            tile = iconCornerSingle[number];
+        }
+        else if (IconTypesHelper.singleFloor.Contains(typeAsInt) && choose == 0)
+        {
+            number = UnityEngine.Random.Range(0, iconSingle.Count);
+            tile = iconSingle[number];
+        }
+        else if (IconTypesHelper.singleOneLose.Contains(typeAsInt)) {
+            number = UnityEngine.Random.Range(0, iconSingleOneLose.Count);
+            tile = iconSingleOneLose[number];
+        }
+
+        if (tile != null)
+            PaintSingleTile(iconTilemap, tile, position);
+    }
+}
